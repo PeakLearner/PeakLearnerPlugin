7d91eea32cdac5f81203a71ddd552d2dd3efce18
Tristan Miller <tem83@nau.edu>
2021-01-19 15:40:31 -0700

    Remove feature log

--- js/View/Track/Model.js
+++ js/View/Track/Model.js
@@ -66,7 +66,6 @@ function (
                             },
                             block.domNode,
                             );
-                        console.log(feature)
                     },
                     () => {},
                     error => {
